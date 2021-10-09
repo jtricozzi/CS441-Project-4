15dfedd3dfd1f8068b2acc58a4405e90c6ee9e81
jtricozzi <42682429+jtricozzi@users.noreply.github.com>
2021-10-09 17:52:27 -0400

    started adding obstacles

--- core/src/com/mygdx/game/CS441Project4.java
+++ core/src/com/mygdx/game/CS441Project4.java
@@ -8,16 +8,23 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
 
 public class CS441Project4 extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	Sprite mainSprite;
+	Texture img, rectImg;
+	Sprite mainSprite, flyingObject;
+	ArrayList<Sprite> obstacleList = new ArrayList<Sprite>();
+
+
 
 	float x, y, dx, dy;
 	float w, h;
 
+	Rectangle rect;
 	Circle mainCir;
 	private boolean moving;
 	
@@ -25,12 +32,18 @@ public class CS441Project4 extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		rectImg = new Texture("rectangleImage.png");
 		mainSprite = new Sprite(img);
+		flyingObject = new Sprite(rectImg);
+
+		//for(int i = 0; )
 
 		x = 0;
 		y = 0;
 		dx = 0;
 		dy = 0;
+
+
 
 		mainCir = new Circle(x, y, mainSprite.getWidth() / 2);
 
@@ -38,14 +51,30 @@ public class CS441Project4 extends ApplicationAdapter {
 			@Override public boolean keyDown (int keycode) {
 				if(keycode == Input.Keys.RIGHT){
 					moving = true;
-					dx = 15;
+					dy = 20;
 				}else if(keycode == Input.Keys.LEFT){
-
+					moving = true;
+					dy = -20;
+				}else if(keycode == Input.Keys.UP){
+					moving = true;
+					dx = -20;
+				}else if(keycode == Input.Keys.DOWN){
+					moving = true;
+					dx = 20;
 				}
 				return true;
 			}
 			@Override public boolean keyUp (int keycode) {
 				if(keycode == Input.Keys.RIGHT){
+					moving = false;
+					dy = 0;
+				}else if(keycode == Input.Keys.LEFT){
+					moving = false;
+					dy = 0;
+				}else if(keycode == Input.Keys.UP){
+					moving = false;
+					dx = 0;
+				}else if(keycode == Input.Keys.DOWN){
 					moving = false;
 					dx = 0;
 				}
@@ -60,13 +89,14 @@ public class CS441Project4 extends ApplicationAdapter {
 		ScreenUtils.clear(0, 0, 0, 1);
 
 		//x = x + dx;
-		y = y + dy;
+		//y = y + dy;
 
 		mainCir.x = x;
 		mainCir.y = y;
 
 		if(moving){
 			x = x + dx;
+			y = y + dy;
 		}
 
 		batch.begin();
